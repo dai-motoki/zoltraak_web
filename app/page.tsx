60b9f9c353e92ce75a87c5dc13a3db940289cdd4
dai-motoki <dai.motoki1123@gmail.com>
2024-05-03 20:45:18 +0900

    add

--- app/page.tsx
+++ app/page.tsx
@@ -95,8 +95,11 @@ export default function Home() {
           </nav>
         </div>
       </div>
+
+
+
       {/* メインコンテンツ */}
-      <div className="ml-64">
+      <div className="ml-64 mt-[-80px]"> {/* 上に少し上げるためにmt-[-20px]を追加 */}
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
           <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
             {/* 創造魔法 zoltraakを始めましょう&nbsp;
