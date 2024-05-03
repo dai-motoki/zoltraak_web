074fb4a27ad279b42704b5209ffa37f7f5e03e45
dai-motoki <dai.motoki1123@gmail.com>
2024-05-03 20:53:26 +0900

    add

--- app/page.tsx
+++ app/page.tsx
@@ -177,7 +177,7 @@ export default function Home() {
             </a>
 
             <a
-              href="/templates"
+              href="https://github.com/dai-motoki/zoltraak/tree/main/zoltraak/grimoires"
               className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
               target="_blank"
               rel="noopener noreferrer"
@@ -189,7 +189,7 @@ export default function Home() {
                 </span>
               </h2>
               <p className="m-0 max-w-[30ch] text-sm opacity-50">
-                zoltraakの魔導書のテンプレートを探しましょう。（未完成）
+                zoltraakの魔導書のテンプレートを探しましょう。（Githubのディレクトリに飛びます）
               </p>
             </a>
 
